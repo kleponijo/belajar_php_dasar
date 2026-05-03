a0b979b58dbb23c468031387a24e7897a4adf348
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-05-03 17:48:05 +0700

    yap

--- 2_TipeDataNumber.php
+++ 2_TipeDataNumber.php
@@ -1,1 +1,31 @@
 <?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b111111);
+
+echo "Underscore in Number : ";
+var_dump(1_234_323_453_232);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point E Notation : (1.7 x 1000) ";
+var_dump(1.7e3);
+
+echo "Floating Point E Notation minus : (1.7 x 0.001) ";
+var_dump(1.7e-3);
+
+echo "Underscore di Floating Point : ";
+var_dump(1_123.123);
+
+echo "Integer Overflow : ";
+var_dump(9223372036854775808);
