d5d80eb46d6b80f60c097af84a8da22d37008aed
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-05-19 07:46:54 +0700

    oke

---
+++ 29_FunctionReturnValue.php
@@ -0,0 +1,34 @@
+<?php
+
+function sum(int $first, int $second)
+{
+  $total = $first + $second;
+  return $total;
+}
+
+$result = sum(10, 10);
+var_dump($result);
+
+$result = sum(20, 20);
+var_dump($result);
+
+function getFinalValue(int $value)
+{
+  if ($value >= 80) {
+    return "A";
+  } else if ($value >= 70) {
+    return "B";
+  } else if ($value >= 60) {
+    return "C";
+  } else if ($value >= 50) {
+    return "D";
+  } else {
+    return "E";
+  }
+}
+
+$score = getFinalValue(90);
+var_dump($score);
+
+$score = getFinalValue(40);
+var_dump($score);
